14db2f91d41b7b45b31ad02f5146cbb68e1ae529
Khloud <62654747+Khloud-M@users.noreply.github.com>
2023-03-28 13:25:50 +0200

    user account

--- src/pages/Account/FavouritePage.vue
+++ src/pages/Account/FavouritePage.vue
@@ -1,7 +1,9 @@
 <template>
-  <div class="heading">
-    <h4>My Favourites</h4>
-    <p>You can control your favorites here</p>
+  <div class="favourite_page">
+    <div class="heading">
+      <h4>My Favourites</h4>
+      <p>You can control your favorites here</p>
+    </div>
   </div>
 </template>
 
@@ -9,5 +11,8 @@
 export default {};
 </script>
 
-<style>
+<style lang="scss">
+.favourite_page {
+  padding: 20px 0;
+}
 </style>
--- src/pages/Account/OrderPage.vue
+++ src/pages/Account/OrderPage.vue
@@ -1,7 +1,9 @@
 <template>
-  <div class="heading">
-    <h4>Orders List</h4>
-    <p>You can control and follow up on your orders here</p>
+  <div class="order_page">
+    <div class="heading">
+      <h4>Orders List</h4>
+      <p>You can control and follow up on your orders here</p>
+    </div>
   </div>
 </template>
 
@@ -9,5 +11,8 @@
 export default {};
 </script>
 
-<style>
+<style lang="scss">
+.order_page {
+  padding: 20px 0;
+}
 </style>
